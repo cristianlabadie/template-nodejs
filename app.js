d07fa397b7458caed8acb8a5f2d61324a1f216af
Cristian Andres Moreno Labadie <42389014+cristianlabadie@users.noreply.github.com>
2023-08-20 20:46:51 -0400

    chore: :test_tube: testeo

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+app.get("*", (req, res) => {
+  res.redirect("/");
+});
+
+app.get("/test", (req, res) => {
+  res.json({
+    hola: "chao",
+  });
+});
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+  console.log(`App listening on port ${port}`);
+});
